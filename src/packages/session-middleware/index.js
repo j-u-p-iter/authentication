bce81d2bcebc0e8bb252035f1802b87a072a575b
kirill <best.portfoleo@gmail.com>
2018-01-27 17:24:49 +0500

    Initial commit

---
+++ src/packages/session-middleware/index.js
@@ -0,0 +1,34 @@
+class SessionMiddleware {
+
+  _parseAuthorizationHeader(header) {
+    return header.split(' ')[1] || null;
+  }
+
+  _getToken(req) {
+    const authorizationHeader = req.headers.authorization;
+
+    return authorizationHeader ? this._parseAuthorizationHeader(authorizationHeader) : null;
+  }
+
+  _decodeToken(token) {
+    return this._authenticationService.decodeToken(token);
+  }
+
+  _parseTokenData(req, res, next) {
+    req.tokenData = this._decodeToken(this._getToken(req)) || {};
+
+    next();
+  }
+
+  constructor(authenticationService) {
+    this._authenticationService = authenticationService;
+  }
+
+  create() {
+    return (req, res, next) =>
+      this._parseTokenData(req, res, next);
+  }
+
+}
+
+export default SessionMiddleware;
