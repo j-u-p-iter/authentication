d8efd9b46acc3e27012ff3c37f7e43f1758b2eeb
kirill <best.portfoleo@gmail.com>
2018-01-31 11:02:15 +0500

    Add config to compile modules

--- src/packages/authentication-service/scripts/build.js
+++ src/packages/authentication-service/scripts/build.js
@@ -1,8 +1,7 @@
 const createBuilder = require('@j.u.p.iter/modules-builder');
 
-const INFO_TO_COMPILE = require('./data/info-to-compile');
+const INFO_TO_COMPILE = require('./configs/compile-config');
 const createBuilds = createBuilder(INFO_TO_COMPILE);
 
 
 createBuilds();
-
